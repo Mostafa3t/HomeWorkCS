5f92f3c924a5b09671fc4bada276e57199d24952
Mostafa3t <202310507@aau.edu.jo>
2024-11-13 18:40:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{                      // Modtafa Firas Otoom 
+                       // ID : 202310507 
+    static void Main()
+    {
+        try
+        {
+            
+            Console.Write("enter minNumber: ");
+            int minNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("enter maxNumber: ");
+            int maxNumber = int.Parse(Console.ReadLine());
+
+
+            if (minNumber > maxNumber)
+            {
+                Console.WriteLine(" minNum should be less than or equal to maxNum.");
+                return;
+            }
+
+            int[] frequency = new int[maxNumber - minNumber + 1];
+
+            Random random = new Random();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                int randomNumber = random.Next(minNumber, maxNumber + 1);
+                frequency[randomNumber - minNumber]++;
+            }
+
+           
+            Console.WriteLine("\nNumber\tFrequency");
+            for (int i = 0; i < frequency.Length; i++)
+            {
+                Console.WriteLine($"{minNumber + i}\t{frequency[i]}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
